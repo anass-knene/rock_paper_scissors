298bdea125d96acd6ee05779b8e4de7707ddafa5
Anass knene <anas.knene@hotmail.com>
2022-11-19 21:38:12 +0100

    create landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
-import React from 'react';
-
+import React from "react";
+import "./styles/App.scss";
+import { BsArrowDown } from "react-icons/bs";
 function App() {
   return (
     <div className="App">
-      <h1>My React App</h1>
+      <div className="rules">
+        <h1>Rules</h1>
+        <BsArrowDown size="4em" style={{ marginBottom: "30px" }} color="red" />
+
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Rock-paper-scissors.svg/1200px-Rock-paper-scissors.svg.png"
+          alt="img"
+          width="600px"
+          height="600px"
+        />
+        <input type="button" value="Get Started" className="StartBtn" />
+      </div>
     </div>
   );
 }
